9f3ab5468e2b38e5b7012b6f0529de57712e008a
Madison Clark <madamber4@gmail.com>
2022-02-07 18:14:53 -0700

    Created the database, built the models, & seeded the database.

---
+++ Mission6/Models/ToDoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        [Required]
+        public int ToDoItemId { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category {get; set;}
+        public bool Completed { get; set; }
+                
+    }
+}
